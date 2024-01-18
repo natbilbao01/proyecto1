cbf797946c2b4ec6e33ada9aeac3f80d57bd9957
nataliaB <mbilbaonat@gmail.com>
2024-01-18 07:17:45 -0500

    cedula

--- edad.py
+++ edad.py
@@ -1,7 +1,6 @@
-nombre =input("ingrese por favor su nombre: ")
+nombre =input("ingrese su numero de cedula: ")
 edad = int(input("ingrese por favor su edad:"))
 if edad >=18:
     print(f"{nombre},Eres mayor de edad")
 else:
     print(f"{nombre},Eres menor de edad")
-    
