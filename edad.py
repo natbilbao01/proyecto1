7c0ec0ee3a4ecb734171638fc99a5683ebae6c67
nataliaB <mbilbaonat@gmail.com>
2024-01-18 06:38:46 -0500

    Se agregaron y se modificaron los archivos edad y par

---
+++ edad.py
@@ -0,0 +1,7 @@
+nombre ="Natalia"
+edad =30
+if edad >=18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+    
---
+++ par.py
@@ -0,0 +1,5 @@
+numero = 20
+if numero %2 ==0:
+    print("El numeroes par")
+else:
+    print("El numero es impar")
